a6d71073eeb2a6933f92e33d0d7c52e8e8bf548d
KSM-public <kamilsmo96@gmail.com>
2022-11-08 22:46:03 +0100

    Initial database connection class implementation

--- TKExercise/MainWindow.xaml.cs
+++ TKExercise/MainWindow.xaml.cs
@@ -20,9 +20,55 @@ namespace TKExercise
     /// </summary>
     public partial class MainWindow : Window
     {
+        SQLServerConnection serverConnection;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            serverConnection = new SQLServerConnection();
+        }
+
+        //Disable loading data if it's not already disabled
+        private void DisableLoading()
+        {
+            if (LoadDataButton.IsEnabled)
+            {
+                LoadDataButton.IsEnabled = false;
+            }
+        }
+
+        private void ButtonCheckConnection_Click(object sender, RoutedEventArgs e)
+        {
+            serverConnection.SetConnectionDetails(TextHost.Text, TextUser.Text, TextPassword.Password);
+
+            if (serverConnection.TryConnect())
+            {
+                MessageBox.Show("Połączenie z bazą danych powiodło się.", "Informacja");
+                LoadDataButton.IsEnabled = true;
+            }    
+            else
+            {
+                MessageBox.Show("Wystąpił błąd podczas połączenia: " + serverConnection.ErrorMessage, "Błąd");
+                DisableLoading();
+            }
+        }
+
+        //Disable loading data if connection details were changed to force user to check connection before trying to load data
+        private void TextConnection_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            DisableLoading();
+        }
+
+        //Same as before
+        private void TextPassword_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            DisableLoading();
+        }
+
+        private void LoadDataButton_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
---
+++ TKExercise/SQLServerConnection.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace TKExercise
+{
+    //Provides methods for connecting to the SQL Server database and getting data
+    internal class SQLServerConnection
+    {
+        string host, user, password; //Database connection details
+        string connectionString; //Connection string
+        string errorMessage; //Contains error message
+
+        public string ErrorMessage { get => errorMessage; }
+
+        //Disable warning that warns about non nullable field that must contain non null variable when exiting constructor
+        //These fields are initialized in separate method but warning still occurs so disable it for clarity
+        #pragma warning disable CS8618
+
+        //Default constructor that doesn't setup anything
+        public SQLServerConnection()
+        {
+            SetConnectionDetails("", "", "");
+        }
+
+        //Constructor that setup connection details
+        public SQLServerConnection(string host, string user, string password)
+        {
+            SetConnectionDetails(host, user, password);
+        }
+
+        //Values are set so restore warning for rest of the code
+        #pragma warning restore CS8618
+
+        //Setup connection details
+        public void SetConnectionDetails(string host, string user, string password)
+        {
+            this.host = host;
+            this.user = user;
+            this.password = password;
+
+            connectionString = "Data Source = " + host + ";Initial Catalog = DevData; User ID = " + user + "; Password = " + password + "";
+        }
+
+        //Try connecting to database and return if it was possible
+        public bool TryConnect()
+        {
+            if (host == "" || user == "" || password == "")
+            {
+                //No connection details so exit with error
+                return false;
+            }
+
+            SqlConnection connection;
+            connection = new SqlConnection(connectionString);
+
+            try 
+            { 
+                connection.Open(); 
+            }
+            catch (Exception ex)
+            {
+                errorMessage = ex.Message;
+                return false;
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return true;
+        }
+    }
+}
